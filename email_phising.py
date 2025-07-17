b5d03611a8156225421f003204622141dae93982
JorgeCerda1401 <jorge.cerda@cetys.edu.mx>
2025-07-16 22:00:04 -0700

    Phising Model train

---
+++ email_phising.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+df = pd.read_csv('spam.csv')
+
+df['Category'] = df['Category'].apply(lambda x: 1 if x == 'spam' else 0)
+
+X_train, X_test, Y_train, Y_test = train_test_split(df['Message'], 
+                                                    df['Category'], 
+                                                    random_state=0)
+
+# Vectorizing text data
+vectorizer = CountVectorizer(ngram_range=(1, 2)).fit(X_train)
+X_train_vectorized = vectorizer.transform(X_train)
+X_train_vectorized.toarray().shape
+
+model = SVC(kernel='linear') 
+model.fit(X_train_vectorized, Y_train)
+
+predictions = model.predict(vectorizer.transform(X_test))
+print("Accuracy:", 100 * sum(predictions == Y_test) / len(predictions), '%')
