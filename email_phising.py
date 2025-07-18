dfe800fa0b30d5a64880a0141b35ed9ac2dcf553
JorgeCerda1401 <jorge.cerda@cetys.edu.mx>
2025-07-17 19:36:38 -0700

    Revert "Saving the model"
    This reverts commit ec6f56b7ffbd8cd13bdebb6907c95c80008ad6db.

--- email_phising.py
+++ email_phising.py
@@ -5,9 +5,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-import joblib
-
-df = pd.read_csv('backend/spam.csv')
+df = pd.read_csv('spam.csv')
 
 df['Category'] = df['Category'].apply(lambda x: 1 if x == 'spam' else 0)
 
@@ -20,11 +18,8 @@ vectorizer = CountVectorizer(ngram_range=(1, 2)).fit(X_train)
 X_train_vectorized = vectorizer.transform(X_train)
 X_train_vectorized.toarray().shape
 
-# Training with Support Vector Machine
 model = SVC(kernel='linear') 
 model.fit(X_train_vectorized, Y_train)
 
 predictions = model.predict(vectorizer.transform(X_test))
-
-# Save model
-joblib.dump(model, "backend/FisherMan.pkl") 
+print("Accuracy:", 100 * sum(predictions == Y_test) / len(predictions), '%')
